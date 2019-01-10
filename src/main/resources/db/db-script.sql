ab5cbd5f4b65d801b7f47870e83beb79830e0b45
Jayanta Mandal <Jay.Speaks@Gmail.Com>
2019-01-10 09:13:55 +0530

    Modified DB-scipt

--- src/main/resources/db/db-script.sql
+++ src/main/resources/db/db-script.sql
@@ -11,3 +11,20 @@ CREATE TABLE `NOTES` (
     `COLOR_CODE`	VARCHAR(10),
     PRIMARY KEY (`ID`)
 );
+
+
+Select * from homelane.NOTES;
+
+Insert into NOTES (NOTE_TITLE, NOTE_TEXT, CREATE_TIME, IMPORTANCE, COLOR_CODE)
+Values
+('Note 1', 'The apples taste good', Now(), 'High', 'Green');
+
+Insert into NOTES (NOTE_TITLE, NOTE_TEXT, CREATE_TIME, IMPORTANCE, COLOR_CODE)
+Values
+('Note 2', 'Spring and Spring-Boot', Now(), 'High', 'Blue');
+
+Insert into NOTES (NOTE_TITLE, NOTE_TEXT, CREATE_TIME, IMPORTANCE, COLOR_CODE)
+Values
+('Note 3', 'JPA and Hibernate', Now(), 'High', 'Blue');
+
+Commit;
